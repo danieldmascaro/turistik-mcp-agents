2105dc467a238db18778df5aa031ea3e0463ccb8
Daniel Díaz Mascaró <ddiazm@turistik.com>
2026-01-07 09:50:30 -0300

    woocommerce testeado

--- agents/src/agents.ts
+++ agents/src/agents.ts
@@ -5,13 +5,13 @@ import { Agent, run, hostedMcpTool } from '@openai/agents';
 const triageAgent = new Agent({
   name: 'Agente de tours',
   instructions:
-    "Buscas tours utilizando herramientas de tu servidor MCP. Das las respuestas en formato\nNombre del tour: (nombre)\nDescripción: (descripción)\nPrecio: (precio)\nDuración: (duración)\n\nSi no hay tours que coincidan con la consulta, respondes con 'No se encontraron tours que coincidan con la consulta.'",
+    "Buscas tours utilizando herramientas de tu servidor MCP. Das las respuestas en formato\nNombre del tour: (nombre)\nDescripción: (descripción)\nPrecio: (precio)\nDuración: (duración si es que aplica)\n\nDas respuestas directas y buscas la información de manera proactiva para promocionar rápidamente los productos con la menor cantidad de interacciones posibles. Si no hay tours que coincidan con la consulta, respondes con 'No se encontraron tours que coincidan con la consulta.'",
   handoffs: [],
-  tools: [hostedMcpTool({serverLabel: 'turistik-mcp-server', serverUrl: 'https://48ed562dc0cf.ngrok-free.app/mcp'})],
+  tools: [hostedMcpTool({serverLabel: 'turistik-mcp-server', serverUrl: 'https://70667003755d.ngrok-free.app/mcp'})],
 });
 
 async function main() {
-  const result = await run(triageAgent, 'Qué tours me recomiendas para ir a la playa?');
+  const result = await run(triageAgent, 'tienen buses hop on hop off?');
   console.log(result);
   console.log('---');
   console.log(result.finalOutput)
